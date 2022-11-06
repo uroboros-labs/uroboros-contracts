ab96f2f13e922b2d22363f01a9971373c1fa9d09
maksfourlife <maksfourlife@gmail.com>
2022-11-06 20:43:21 +0300

    can't remember, new branch for new parts and partial reverts

--- contracts/UroborusRouter.sol
+++ contracts/UroborusRouter.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
 import "./interfaces/IAdaptor.sol";
+
 import "./libraries/Hex.sol";
 import "./libraries/Math.sol";
 import "./libraries/SafeMath.sol";
@@ -20,7 +21,6 @@ contract UroborusRouter {
 	using Math for uint256;
 	using SafeMath for uint256;
 
-	/// @dev used to compute adaptorAddress: keccak256(rlp([adaptorDeployer, adaptorId]))[12:]
 	address public immutable adaptorDeployer;
 
 	constructor(address _adaptorDeployer) {
@@ -37,37 +37,26 @@ contract UroborusRouter {
 		bytes data;
 	}
 
-	/// Executes route: first estimates route amounts, then executes successfully estimated sections
 	function executeRoute(Part[] memory route, address[] memory tokens)
 		external
 		payable
 		returns (uint256[] memory, uint256)
 	{
 		(uint256[] memory amounts, uint256 skip) = simulateRoute(route, tokens);
+		_callExecuteSection(route, tokens, skip, 0);
 		return (amounts, skip);
 	}
 
-	/// Simulates route using adapter's ".quote" method. Costs much less gas because of read-only calls.
-	/// @return amounts estimated amounts
-	/// @return skip skip mask
 	function simulateRoute(Part[] memory route, address[] memory tokens)
 		internal
 		view
 		returns (uint256[] memory, uint256)
 	{
 		uint256[] memory amounts = new uint256[](route.length);
-		// uint256[] memory balances = new uint256[](tokens.length);
 		uint256 skip;
 		for (uint256 i; i < route.length; i++) {
 			if (skip & (1 << route[i].sectionId) != 0) continue;
 			uint256 amountIn = route[i].amountIn;
-			// uint256 balance = balances[route[i].tokenInId];
-			// if (amountIn == 0) {
-			// 	amountIn = balance;
-			// } else if (amountIn > balance) {
-			// 	balances[route[i].tokenInId] = amountIn;
-			// }
-			// if amountIn=0, find balance
 			if (amountIn == 0)
 				for (uint256 j; j < i; j++) {
 					if (skip & (1 << route[j].sectionId) != 0) continue;
@@ -77,36 +66,39 @@ contract UroborusRouter {
 						else amountIn -= route[j].amountIn;
 					}
 				}
-			console.log(
-				"tokenIn: %s, amountIn: %s",
-				IERC20Metadata(tokens[route[i].tokenInId]).symbol(),
-				amountIn
-			);
 			amounts[i] = IAdaptor(route[i].adaptor).quote(
 				tokens[route[i].tokenInId],
 				amountIn,
 				route[i].data
 			);
-			if (amounts[i] < route[i].amountOutMin) {
-				skip |= (1 << route[i].sectionId);
-				continue;
-			}
-			// if we are at the end of cycle, we previously substracted amountIn and didn't restored it
-			// balances[route[i].tokenInId] -= amountIn;
-			// balances[route[i].tokenOutId] += amounts[i];
+			if (amounts[i] < route[i].amountOutMin) skip |= (1 << route[i].sectionId);
 		}
 		return (amounts, skip);
 	}
 
-	/// Recursively executes nested sections, returns amounts
-	/// @notice for internal use purposes only
-	/// @dev should be called from 'executeRoute' function or from itself
+	function _callExecuteSection(
+		Part[] memory section,
+		address[] memory tokens,
+		uint256 skip,
+		uint256 sectionId
+	) internal returns (uint256[] memory) {
+		bytes memory data = abi.encodeWithSelector(
+			this._executeSection.selector,
+			section,
+			tokens,
+			skip,
+			sectionId
+		);
+		(, data) = address(this).delegatecall(data);
+		return abi.decode(data, (uint256[]));
+	}
+
 	function _executeSection(
 		Part[] memory section,
 		address[] memory tokens,
-		uint256 skip
+		uint256 skip,
+		uint256 sectionId
 	) external returns (uint256[] memory) {
-		uint256 sectionId = section[0].sectionId;
 		uint256[] memory amounts = new uint256[](section.length);
 		for (uint256 i; i < section.length; ) {
 			if (section[i].sectionId > sectionId) {
@@ -115,110 +107,18 @@ contract UroborusRouter {
 				uint256 nextLength = j - i;
 				Part[] memory nextSection = new Part[](nextLength);
 				for (uint256 k; k < nextLength; k++) nextSection[k] = section[i + k];
-				uint256[] memory nextAmounts = this._executeSection(nextSection, tokens, skip); // try
+				uint256[] memory nextAmounts = this._executeSection(
+					nextSection,
+					tokens,
+					skip,
+					section[i].sectionId
+				);
 				for (uint256 k; k < nextLength; k++) amounts[i + k] = nextAmounts[k];
 				i += nextLength;
 			} else {
-				// swap here
 				i++;
 			}
 		}
-		// for (uint256 i; i < section.length; ) {
-		// 	if (skip & (1 << section[i].sectionId) != 0) continue;
-		// 	if (section[i].sectionId != sectionId) {
-		// 		// steps to the end of the section
-		// 		i = _executeNextSection(section, tokens, amounts, skip, i);
-		// 	} else {
-		// 		_swapPart(section, tokens, amounts, i);
-		// 		i++;
-		// 	}
-		// }
-
 		return amounts;
 	}
-
-	function _swapPart(
-		Part[] memory section,
-		address[] memory tokens,
-		uint256[] memory amounts,
-		uint256 i
-	) internal {
-		uint256 amountIn = section[i].amountIn;
-		address tokenIn = tokens[section[i].tokenInId];
-		address tokenOut = tokens[section[i].tokenOutId];
-		// if parts' amountIn not specified, use all avail. balance
-		// else if amountIn is greater than avail., transfer difference
-		if (amountIn == 0) {
-			amountIn = IERC20(tokenIn).selfBalance();
-		}
-		bytes memory data = abi.encodeWithSelector(
-			IAdaptor.swap.selector,
-			tokenIn,
-			amountIn,
-			section[i].data
-		);
-		bool success;
-		uint256 preBalance = IERC20(tokenOut).selfBalance();
-		(success, ) = section[i].adaptor.delegatecall(data);
-		require(success);
-		uint256 postBalance = IERC20(tokenOut).selfBalance();
-		amounts[i] = postBalance.sub(preBalance);
-		if (amounts[i] < section[i].amountOutMin) {
-			assembly {
-				revert(add(0x20, amounts), mload(amounts))
-			}
-		}
-	}
-
-	function _executeNextSection(
-		Part[] memory section,
-		address[] memory tokens,
-		uint256[] memory amounts,
-		uint256 skip,
-		uint256 i
-	) internal returns (uint256) {
-		Part[] memory nextSection = _getNextSection(section, i);
-		bytes memory data = abi.encodeWithSelector(
-			this._executeSection.selector,
-			nextSection,
-			tokens,
-			skip
-		);
-		bool success;
-		(success, data) = address(this).delegatecall(data);
-		uint256[] memory nextAmounts = abi.decode(data, (uint256[]));
-		uint256 j;
-		for (; j < nextAmounts.length; j++) amounts[i + j] = nextAmounts[j];
-		return i + j;
-	}
-
-	/// @param section currently executed section
-	/// @param i current part index
-	/// @return nextSection next section
-	/// @notice called when current parts' sectionId not equals to previos.
-	function _getNextSection(Part[] memory section, uint256 i)
-		internal
-		pure
-		returns (Part[] memory)
-	{
-		uint256 j;
-		for (; j < section.length; j++) if (section[j].sectionId != section[i].sectionId) break;
-		uint256 nextLength = j - i;
-		Part[] memory nextSection = new Part[](nextLength);
-		for (j = 0; j < nextLength; j++) nextSection[j] = section[i + j];
-		return nextSection;
-	}
-
-	// function _getRouteId(
-	// 	Part[] memory route,
-	// 	address[] memory tokens,
-	// 	uint256 skip
-	// ) internal pure returns (bytes32) {
-	// 	// +1 for out token
-	// 	uint256 length = route.length + 1;
-	// 	// goes through skip mask and subsracts skipped
-	// 	for (uint256 i; i < route.length; i++) if (skip & (1 << i) != 0) length--;
-	// 	address[] memory routeTokens = new address[](length);
-	// 	uint256 j;
-	// }
 }
